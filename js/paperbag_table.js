1476f37e9fcb2d18ee822e9744032d08df1a9891
William F. Broderick <billbrod@gmail.com>
2019-01-07 19:17:33 -0500

    Small layout changes
     - removes alert
    
     - changes indentation of code
    
     - height is now 100% of screen, not fixed
    
     - layout switched to fitColumns (so columns will adjust to fill
       width), when paired with widthGrow attribute in columns
    
     - removes pagination (which was responsible for those buttons)
    
     - sets the columns we want hidden to have minWidth:2000, those
       hopefully always hiding them. this is pretty hacky, but couldn't
       find a way to tell Tabulator to always hide them

--- js/paperbag_table.js
+++ js/paperbag_table.js
@@ -10,7 +10,6 @@ function init() {
 
 function showInfo(data, tabletop) {
 
-    alert('Successfully processed!')
 	console.log(data);
 
 	var table = new Tabulator("#paperbag_table", {
@@ -18,48 +17,48 @@ function showInfo(data, tabletop) {
     	responsiveLayout:"collapse",
     	responsiveLayoutCollapseStartOpen:false,
     	responsiveLayoutCollapseFormatter:function(data){
-        var list = document.createElement("ul");
+            var list = document.createElement("ul");
 
-        for(var key in data){
-            let item = document.createElement("li");                
-            //item.innerHTML = "<strong>" + key + "</strong> - " + data[key];
-            item.innerHTML = "<strong>" + key + "</strong>" + "<pre><small>" + data[key] + "</small></pre>";
-            list.appendChild(item);
-        }
+            for(var key in data){
+		let item = document.createElement("li");                
+		//item.innerHTML = "<strong>" + key + "</strong> - " + data[key];
+		item.innerHTML = "<strong>" + key + "</strong>" + "<pre><small>" + data[key] + "</small></pre>";
+		list.appendChild(item);
+            }
 
-        return Object.keys(data).length ? list : "";
-    },
+            return Object.keys(data).length ? list : "";
+	},
 
-		height:"650px",
-		tooltips:true,
-		addRowPos:"top",
-		history:true,
-		pagination:"local",
-		paginationSize:20,
-		movableColumns:true,
-		resizableRows:true,
-		data:data,
-		initialSort: [
-			{column:"name", dir:"asc"},
-		],
-		columns:[
-			{formatter:"responsiveCollapse", width:30, minWidth:30, align:"center", headerSort:false},
-			{title:"Location", field:"Locale of Study", formatter:"textarea", width:200},
-			{title:"Summary", field:"Paragraph Summary", formatter:"textarea",  width:400},
-			{title:"Type", field:"Type of Fee", formatter:"textarea", width:100},
-			{title:"&#162;", field:"Amount of Fee", formatter:"textarea", width:80},
-			{title:"Study Author", field:"Study Author", formatter:"textarea", width:200},
-		    {title:"Date of study", field:"Date of study", formatter:"textarea", width:150},
-			//{title:"Date", field:"Date that ordinance came into effect", formatter:"textarea", width:240},
-			//{title:"Study Author", field:"Type of Study Author", formatter:"textarea", width:300},
-			{title:"Study Methodology", field:"Study Methodology", formatter:"textarea", width:400},
-			//{title:"Key Manipulation", field:"Key Manipulation", formatter:"textarea", width:300},
-			{title:"Key Stats", field:"Key Stats", formatter:"textarea", width:600},
-			{title:"Destination of fee", field:"Destination of fee", formatter:"textarea", width:300},
-			{title:"Link to Study", field:"Link to Study or Official Website", formatter:"textarea", width:300},
-			{title:"Link to Law", field:"Link to Text of Law", formatter:"textarea", width:300},
-			//{title:"Other Notes", field:"Other Notes", formatter:"textarea", width:300},
-		]
+	    height:"100%",
+     	    layout:"fitColumns",
+	    tooltips:true,
+	    addRowPos:"top",
+	    history:true,
+	    pagination:false,
+	    movableColumns:true,
+	    resizableRows:true,
+	    data:data,
+	    initialSort: [
+		{column:"name", dir:"asc"},
+	    ],
+	    columns:[
+		{formatter:"responsiveCollapse", width:30, minWidth:30, align:"center", headerSort:false},
+		{title:"Location", field:"Locale of Study", formatter:"textarea", widthGrow:1},
+		{title:"Summary", field:"Paragraph Summary", formatter:"textarea",  widthGrow:3, minWidth:500},
+		{title:"Type", field:"Type of Fee", formatter:"textarea", widthGrow:1},
+		{title:"&#162;", field:"Amount of Fee", formatter:"textarea", widthGrow:1},
+		{title:"Study Author", field:"Study Author", formatter:"textarea", widthGrow:2},
+		{title:"Date of study", field:"Date of study", formatter:"textarea", widthGrow:1},
+		//{title:"Date", field:"Date that ordinance came into effect", formatter:"textarea", width:240},
+		//{title:"Study Author", field:"Type of Study Author", formatter:"textarea", width:300},
+		{title:"Study Methodology", field:"Study Methodology", formatter:"textarea", minWidth:2000},
+		//{title:"Key Manipulation", field:"Key Manipulation", formatter:"textarea", width:300},
+		{title:"Key Stats", field:"Key Stats", formatter:"textarea", minWidth:2000},
+		{title:"Destination of fee", field:"Destination of fee", formatter:"textarea", minWidth:2000},
+		{title:"Link to Study", field:"Link to Study or Official Website", formatter:"textarea", minWidth:2000},
+		{title:"Link to Law", field:"Link to Text of Law", formatter:"textarea", minWidth:2000},
+		//{title:"Other Notes", field:"Other Notes", formatter:"textarea", width:300},
+	    ]
 	});
 }
 
