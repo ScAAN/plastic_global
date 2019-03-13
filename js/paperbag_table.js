2aa52ed2aa52debd4e17760f2fa2c55ecc6d07ca
William F. Broderick <billbrod@gmail.com>
2019-03-13 10:57:24 -0400

    Changes to global plastic bag laws

--- js/paperbag_table.js
+++ js/paperbag_table.js
@@ -1,4 +1,4 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1BFSzZD2bQmfV3TprO7neGChxdPsYKwPCTUv_O69OTFE/edit#gid=0'
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1Ii4utQR9v4GYy2NOSBY_qUtMefyrPanh8m2ICiBdHQo/edit?usp=sharing'
 
 function init() {
     Tabletop.init({ 
